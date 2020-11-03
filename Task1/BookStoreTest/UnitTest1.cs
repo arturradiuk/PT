03852cb790e220ed6fa3fc7923abea33b14e30f7
arturradiuk <arturradiuk@gmail.com>
2020-11-03 15:24:48 +0200

    UnitTest1.cs modified

--- Task1/BookStoreTest/UnitTest1.cs
+++ Task1/BookStoreTest/UnitTest1.cs
@@ -9,8 +9,7 @@ namespace BookStoreTest
         [Fact]
         public void Test1()
         {
-            Book book = new Book(new Guid(),"bookName","authorName",2001,Genre.Biography );
-            Assert.Equal(book,book);
+            
         }
     }
 }
