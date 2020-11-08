d997e164762a59587a664120c2e66500008ac5bf
Shykial <szymon.jeziorski@interia.pl>
2020-11-08 21:41:20 +0100

    rest of books test region tests implemented

--- Task1/BookStoreTest/DataServiceTest.cs
+++ Task1/BookStoreTest/DataServiceTest.cs
@@ -44,7 +44,7 @@ namespace BookStoreTest
             ConstantDataFiller constantDataFiller = new ConstantDataFiller();
             DataRepository dataRepository = new DataRepository(constantDataFiller);
             DataService dataService = new DataService(dataRepository);
-            
+
             Book book = dataRepository.GetBook(1);
             var invoices = dataService.GetInvoicesForTheBook(book);
             foreach (var invoice in invoices)
@@ -74,7 +74,116 @@ namespace BookStoreTest
 
             Assert.Equal(dataService.GetInvoices().ToImmutableHashSet().Count, totalInvoices);
         }
-        
+
+        [Fact]
+        public void GetInvoicesForTheBooksAuthorNameTest()
+        {
+            ConstantDataFiller constantDataFiller = new ConstantDataFiller();
+            DataRepository dataRepository = new DataRepository(constantDataFiller);
+            DataService dataService = new DataService(dataRepository);
+
+            String author = dataRepository.GetBook(0).AuthorName;
+
+            var authorBooks = dataService.GetInvoicesForTheBooksAuthorName(author);
+
+            foreach (var book in authorBooks)
+            {
+                Assert.Equal(author, book.AuthorName);
+            }
+        }
+
+        [Fact]
+        public void AddBookTest()
+        {
+            ConstantDataFiller constantDataFiller = new ConstantDataFiller();
+            DataRepository dataRepository = new DataRepository(constantDataFiller);
+            DataService dataService = new DataService(dataRepository);
+
+            Book book = new Book("W pustyni i w puczczy", "Henryk Sienkiewicz", 1910);
+
+            int booksNumber = dataService.GetBooks().ToImmutableHashSet().Count;
+            dataService.AddBook(book);
+            Assert.Equal(booksNumber + 1, dataService.GetBooks().ToImmutableHashSet().Count);
+            Assert.Equal(book, dataService.GetBooks().Last());
+        }
+
+        [Fact]
+        public void GetBookTest()
+        {
+            ConstantDataFiller constantDataFiller = new ConstantDataFiller();
+            DataRepository dataRepository = new DataRepository(constantDataFiller);
+            DataService dataService = new DataService(dataRepository);
+            Book presentBook = dataRepository.GetBook(0);
+            String presentBookName = presentBook.BookName;
+            String presentBookAuthor = presentBook.AuthorName;
+            int presentBookYear = presentBook.Year;
+
+            // Book x = new Book(presentBookName, presentBookAuthor, presentBookYear);
+            // Assert.Equal(x, presentBook);
+
+            Book notPresentBook = new Book("I don't exist", "Neither do I", 2999);
+
+            Assert.Equal(presentBook, dataService.GetBook(presentBookName, presentBookAuthor, presentBookYear));
+            try
+            {
+                Assert.NotEqual(notPresentBook, dataService.GetBook("I don't exist", "Neither do I", 2999));
+            }
+            catch (System.ArgumentException)
+            {
+            }
+        }
+
+        [Fact]
+        public void GetBooksTest()
+        {
+            ConstantDataFiller constantDataFiller = new ConstantDataFiller();
+            DataRepository dataRepository = new DataRepository(constantDataFiller);
+            DataService dataService = new DataService(dataRepository);
+
+            var allBooks = dataService.GetBooks();
+
+            Assert.Equal(dataRepository.GetAllBooks().ToImmutableHashSet().Count, allBooks.ToImmutableHashSet().Count);
+        }
+
+        [Fact]
+        public void UpdateBookTest()
+        {
+            ConstantDataFiller constantDataFiller = new ConstantDataFiller();
+            DataRepository dataRepository = new DataRepository(constantDataFiller);
+            DataService dataService = new DataService(dataRepository);
+
+            Book book = dataRepository.GetBook(0);
+            book.BookName = "W pustyni i w puszczy";
+
+            dataService.UpdateBook(book);
+
+            Assert.Equal("W pustyni i w puszczy", dataService.GetBooks().First().BookName);
+        }
+
+        [Fact]
+        public void DeleteBookTest()
+        {
+            ConstantDataFiller constantDataFiller = new ConstantDataFiller();
+            DataRepository dataRepository = new DataRepository(constantDataFiller);
+            DataService dataService = new DataService(dataRepository);
+
+            Book book = new Book("W pustyni i w puczczy", "Henryk Sienkiewicz", 1910);
+            dataService.AddBook(book);
+
+            int originalCount = dataService.GetBooks().ToImmutableHashSet().Count;
+
+            dataService.DeleteBook(book);
+
+            Assert.Equal(originalCount - 1, dataService.GetBooks().ToImmutableHashSet().Count);
+            try
+            {
+                Assert.NotEqual(book, dataService.GetBook("W pustyni i w puczczy", "Henryk Sienkiewicz", 1910));
+            }
+            catch (System.ArgumentException)
+            {
+            }
+        }
+
         #endregion
     }
 }
