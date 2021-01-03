fe84b460ff61ae0565de8e5676d985579e8f0b76
arturradiuk <arturradiuk@gmail.com>
2021-01-03 09:26:24 +0200

    Two custom extension methods has been implemented

---
+++ Task3/Task3/CustomExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task3
+{
+    public static class CustomExtensionMethods
+    {
+        public static List<Product> GetProductsWithoutCategory_QS(this List<Product> products)
+        {
+            return (from product in products where product.ProductSubcategoryID == null select product).ToList();
+        }
+
+        public static List<Product> GetProductsWithoutCategory_MS(this List<Product> products)
+        {
+            return products.Where(product => product.ProductSubcategoryID == null).ToList();
+        }
+    }
+}
