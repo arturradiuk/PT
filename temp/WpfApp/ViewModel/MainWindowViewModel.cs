0e5bac7fc268ece64b972bbd28696a3c0d7ced40
arturradiuk <arturradiuk@gmail.com>
2021-01-17 23:06:19 +0200

    bug fixes

---
+++ temp/WpfApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Model;
+
+namespace ViewModel
+{
+
+        public class MainWindowViewModel : ViewModelListener
+        {
+            public ICommand UpdateDepartmentCommand { get; private set; }
+            public ICommand DeleteDepartmentCommand { get; private set; }
+            public ICommand AddWindowCommand { get; private set; }
+            public ICommand RefreshWindowCommand { get; private set; }
+
+            private void Refresh()
+            {
+                // Department department = new Department();
+            }
+
+            public MainWindowViewModel()
+            {
+            DepartmentEntity department = new DepartmentEntity();
+            }
+
+
+        public void UpdateDepartment()
+            {
+                Task.Run(() => { });
+            }
+
+            public void DeleteDepartment()
+            {
+                Task.Run(() => { });
+            }
+
+
+            public void AddWindow()
+            {
+                Task.Run(() => { });
+            }
+
+            public void RefreshWindow()
+            {
+                Task.Run(() => { });
+            }
+
+        }
+    }
+
