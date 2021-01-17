eb8023a6a666aad5053c2d632ac32efb9dcd48e4
Shykial <szymon.jeziorski@interia.pl>
2021-01-17 15:02:51 +0100

    Simple name validation added

---
+++ WpfApp/View/Validators/NameValidation.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+
+namespace View
+{
+    public class NameValidation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            string name = value as string;
+            bool isValid = Regex.IsMatch(name ?? string.Empty, @"[\w\s]{2,50}");
+            if (!isValid)
+                return new ValidationResult(false, "value must contain of 2-50 non-special characters only");
+            return ValidationResult.ValidResult;;
+        }
+    }
+}
