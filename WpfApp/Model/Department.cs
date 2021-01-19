3c788dcdd6df749e96bf19aea43263cdf982056a
arturradiuk <arturradiuk@gmail.com>
2021-01-19 19:06:59 +0200

    List view is working correctly and shows departments

--- WpfApp/Model/Department.cs
+++ WpfApp/Model/Department.cs
@@ -2,9 +2,9 @@
 {
     public class Department
     {
-        public short DepartmentID;
-        public string Name;
-        public string GroupName;
-        public System.DateTime ModifiedDate;
+        public short DepartmentID { get; set; }
+        public string Name { get; set; }
+        public string GroupName { get; set; }
+        public System.DateTime ModifiedDate { get; set; }
     }
 }
